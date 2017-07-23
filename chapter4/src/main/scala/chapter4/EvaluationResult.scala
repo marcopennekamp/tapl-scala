dc8c382af06d862fde8812dd72ad0ad7daafa1de
Marco Pennekamp <marco.pennekamp96@gmail.com>
2017-07-23 18:55:30 +0200

    Add big-step semantics.

--- chapter4/src/main/scala/chapter4/EvaluationResult.scala
+++ chapter4/src/main/scala/chapter4/EvaluationResult.scala
@@ -1,13 +1,27 @@
 package chapter4
 
-trait EvaluationResult
+trait EvaluationResult {
+  def flatMap(f: Term => EvaluationResult): EvaluationResult
+  def map(f: Term => Term): EvaluationResult = flatMap(t => EvaluationSuccess(f(t)))
+
+  def evaluateOrFailWith(failResult: Term)(f: Term => EvaluationResult): EvaluationResult = {
+    flatMap(f) match {
+      case s@EvaluationSuccess(_) => s
+      case EvaluationFailure(_) => EvaluationFailure(failResult)
+    }
+  }
+}
 
 /**
   * @param rest The remaining term that couldn't be reduced further.
   */
-case class EvaluationFailure(rest: Term) extends EvaluationResult
+case class EvaluationFailure(rest: Term) extends EvaluationResult {
+  override def flatMap(f: Term => EvaluationResult): EvaluationResult = this
+}
 
 /**
   * @param result The result value of the evaluation.
   */
-case class EvaluationSuccess(result: Term) extends EvaluationResult
+case class EvaluationSuccess(result: Term) extends EvaluationResult {
+  override def flatMap(f: Term => EvaluationResult): EvaluationResult = f(result)
+}
